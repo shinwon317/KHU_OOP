c9016d6fc8b9489c2d47ecaeba1307f15e9572bb
shinwon317 <163814976+shinwon317@users.noreply.github.com>
2024-05-16 18:39:45 +0900

    Create exercise2_store.h

---
+++ hw11/exercise2_store.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "fruit.h"
+class store {
+public:
+   store() {};
+   store(int a, int b, int c, float d, float e, float f, float g, float h) {
+      apple = fruit(a, e);
+      pear = fruit(b, f);
+      peach = fruit(c, g);
+      mango = fruit(d, h);
+
+   }
+   fruit apple, pear, peach, mango;
+   float total();
+};
