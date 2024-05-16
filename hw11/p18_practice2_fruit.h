7abef0e1e2c8f1188e3078e2f31e90bc94d22dca
shinwon317 <163814976+shinwon317@users.noreply.github.com>
2024-05-16 18:44:47 +0900

    Create p18_practice2_fruit.h

---
+++ hw11/p18_practice2_fruit.h
@@ -0,0 +1,18 @@
+#pragma once
+class fruit {
+public:
+    // constructor
+    fruit() {
+        price = 200; many = 2; // default values
+    }
+    fruit(float n, int p) {
+        price = p;
+        many = n;
+    }
+    // member data
+    int many;
+    float price;
+    // member function
+    float show();
+    void show_all();
+};
